339e13ec6d3f00f78be60c5542a7db6b731d2b2e
Paulina <papogorzelska@gmail.com>
2020-04-06 18:52:15 +0200

    image

--- src/components/home/CharactersList.vue
+++ src/components/home/CharactersList.vue
@@ -2,6 +2,8 @@
 <div>   
         <div v-for="pokemon in getCharacters.results" >
          {{pokemon.name}}
+         {{pokemon.id}}
+         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png">
         <SingleItem :url="pokemon.url"></SingleItem>
        </div>
 </div>
